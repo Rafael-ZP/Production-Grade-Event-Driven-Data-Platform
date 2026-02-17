84f4c670ac19e9a2d9e9240b37e25784e34dfa75
Rafael Zieganpalg <rafaelzieganpalg@gmail.com>
2026-02-17 19:57:35 +0530

    Dmail

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -41,7 +41,7 @@ export default function RegisterPage() {
                 onChange={(e) => setUsername(e.target.value)}
               />
               <Input
-                label="Smail"
+                label="Dmail"
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
